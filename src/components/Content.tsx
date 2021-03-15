3d815dc13814fae105ec26a2975dc5a8a13c9644
mcarolmartins <carol-martins7@hotmail.com>
2021-03-15 20:06:26 -0300

    separar componentes content e sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface Props {
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+  selectedGenre: string;
+}
+
+export function Content({ movies, selectedGenre }: Props) {
   // Complete aqui
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  );
 }
